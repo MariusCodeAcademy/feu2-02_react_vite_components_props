0cc50c08128d28fac26cfed5ba028f187c8245e8
Marius <marius@example.com>
2022-11-22 10:15:57 +0200

    service cards section

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,18 @@ function SectionTitle(props) {
 
 function Icon(props) {
   // console.log('props ===', props);
-  return <i className={`fa ${props.iconName} fa-3x`} aria-hidden='true'></i>;
+  return <i className={`fa ${props.iconName} fa-3x card__icon`} aria-hidden='true'></i>;
 }
 
 function Grid() {
   return (
     <div className='grid'>
       <SeviceCard title='Business Consulting' icon='fa-address-book' link='/about.html' />
-      <SeviceCard />
-      <SeviceCard />
+      <SeviceCard title='Market Analysis' icon='fa-envelope-open' link='/home.html' />
+      <SeviceCard title='User Monitoring' icon=' fa-eercast' link='/contact.html' />
+      <SeviceCard title='Business Consulting' icon='fa-address-book' link='/about.html' />
+      <SeviceCard title='Market Analysis' icon='fa-envelope-open' link='/home.html' />
+      <SeviceCard title='User Monitoring' icon=' fa-eercast' link='/contact.html' />
     </div>
   );
 }
@@ -23,7 +26,7 @@ function Grid() {
 function SeviceCard(props) {
   return (
     <div className='card'>
-      <Icon className='card__icon' iconName={props.icon} />
+      <Icon iconName={props.icon} />
       <h3>{props.title}</h3>
       <p className='card__text'>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos dolor atque exercitationem
