fe7bd9cf19d25f8fb97f167d170a9dd02dd222ce
Marius <marius@example.com>
2022-11-22 10:30:45 +0200

    Icon UI

--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -1,11 +1,8 @@
+import Icon from './UI/Icon';
+
 function SectionTitle(props) {
   // console.log('props ===', props);
   return <h2 className='sectionTitle'>{props.title}</h2>;
-}
-
-function Icon(props) {
-  // console.log('props ===', props);
-  return <i className={`fa ${props.iconName} fa-3x card__icon`} aria-hidden='true'></i>;
 }
 
 function Grid() {
