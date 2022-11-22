0cf3c174272c49d181cb4c29db64fe66eb314bc1
Marius <marius@example.com>
2022-11-22 14:33:05 +0200

    render class

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,8 +3,20 @@ import './hero.css';
 function Hero(props) {
   const heroClasses = props.color === 'red' ? 'hero red' : 'hero';
 
+  function caclulateClass() {
+    let rezClass = 'hero ';
+    if (props.color === 'red') {
+      rezClass = rezClass + 'red';
+    }
+    if (props.color === 'green') {
+      rezClass = rezClass + 'green';
+    }
+
+    return rezClass;
+  }
+
   return (
-    <div className={heroClasses}>
+    <div className={caclulateClass()}>
       <div className='container'>
         <h2>{props.title}</h2>
         <h3>{props.subtitle}</h3>
