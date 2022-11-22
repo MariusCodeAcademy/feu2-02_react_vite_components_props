e2ee45966ff90858b853d1383da62ff2b9417e80
Marius <marius@example.com>
2022-11-22 14:26:37 +0200

    dynamic class

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,8 +1,10 @@
 import './hero.css';
 
 function Hero(props) {
+  const heroClasses = props.color === 'red' ? 'hero red' : 'hero';
+
   return (
-    <div className='hero'>
+    <div className={heroClasses}>
       <div className='container'>
         <h2>{props.title}</h2>
         <h3>{props.subtitle}</h3>
