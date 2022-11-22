0a1b5996009e4c6561209b89821c790e40120e59
Marius <marius@example.com>
2022-11-22 09:30:12 +0200

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import './App.css';
 
+function SectionTitle(props) {
+  return <h2 className='sectionTitle'>Our services</h2>;
+}
+
+function Icon() {
+  return <i className='fa fa-address-book' aria-hidden='true'></i>;
+}
+
+function Grid() {
+  return (
+    <div className='grid'>
+      <h2>
+        kortele <Icon />
+      </h2>
+      <h2>
+        kortele <Icon />
+      </h2>
+      <h2>
+        kortele <Icon />
+      </h2>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className='App container'>
-      <h1>Hello vite React !!!</h1>
+      <SectionTitle title='Musu paslaugos' />
+      <Grid />
     </div>
   );
 }
