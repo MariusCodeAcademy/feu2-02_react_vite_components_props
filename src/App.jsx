f10622320178460b2ab4042a1b6b9c6d5d7cb7b8
Marius <marius@example.com>
2022-11-22 09:45:25 +0200

    service3 card

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
 import './App.css';
 
 function SectionTitle(props) {
-  console.log('props ===', props);
+  // console.log('props ===', props);
   return <h2 className='sectionTitle'>{props.title}</h2>;
 }
 
 function Icon(props) {
+  // console.log('props ===', props);
   return <i className={`fa ${props.iconName}`} aria-hidden='true'></i>;
 }
 
 function Grid() {
   return (
     <div className='grid'>
-      <h2>
-        kortele <Icon iconName='fa-address-book' />
-      </h2>
-      <h2>
-        kortele <Icon iconName='fa-envelope-open' />
-      </h2>
-      <h2>
-        kortele <Icon iconName='fa-meetup' />
-      </h2>
+      <SeviceCard />
+      <SeviceCard />
+      <SeviceCard />
+    </div>
+  );
+}
+
+function SeviceCard() {
+  return (
+    <div className='card'>
+      <Icon className='card__icon' iconName='fa-address-book' />
+      <h3>title</h3>
+      <p className='card__text'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos dolor atque exercitationem
+      </p>
+      <a href='#' className='card__link'>
+        Learn more
+      </a>
     </div>
   );
 }
