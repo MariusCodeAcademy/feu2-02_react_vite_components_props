108722de649304945517196866ce0b7c72b02835
Marius <marius@example.com>
2022-11-23 11:21:45 +0200

    cardsArr.map

--- src/components/ServicesGrid.jsx
+++ src/components/ServicesGrid.jsx
@@ -1,19 +1,18 @@
 import SeviceCard from './SeviceCard';
 
 const cardsArr = [
-  { title: 'Business Consulting', icon: 'fa-address-book', link: '/about.html' }, // 0
-  { title: 'Market Analysis', icon: 'fa-envelope-open', link: '/home.html' }, // 1
-  { title: 'User Monitoring', icon: 'fa-eercast', link: '/contact.html' }, // 2
-  { title: 'Facebook', icon: 'fa-facebook', link: '/fb.html' }, // 2
+  { id: 1, title: 'Business Consulting', icon: 'fa-address-book', link: '/about.html' }, // 0
+  { id: 2, title: 'Market Analysis', icon: 'fa-envelope-open', link: '/home.html' }, // 1
+  { id: 3, title: 'User Monitoring', icon: 'fa-eercast', link: '/contact.html' }, // 2
+  { id: 4, title: 'Facebook', icon: 'fa-facebook', link: '/fb.html' }, // 2
 ];
 
 function ServicesGrid() {
   return (
     <div className='grid'>
-      <SeviceCard title={cardsArr[0].title} icon={cardsArr[0].icon} link={cardsArr[0].link} />
-      <SeviceCard title={cardsArr[1].title} icon={cardsArr[1].icon} link={cardsArr[1].link} />
-      <SeviceCard title={cardsArr[2].title} icon={cardsArr[2].icon} link={cardsArr[2].link} />
-      <SeviceCard title={cardsArr[3].title} icon={cardsArr[3].icon} link={cardsArr[3].link} />
+      {cardsArr.map((cObj) => (
+        <SeviceCard key={cObj.id} title={cObj.title} icon={cObj.icon} link={cObj.link} />
+      ))}
     </div>
   );
 }
